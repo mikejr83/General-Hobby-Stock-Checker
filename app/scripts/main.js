241e3892290ca2b9eabc263b997cb761ee9bc5e9
mgardner <mgardner@sumtotalsystems.com>
2015-01-07 12:40:44 -0500

    Fixed up the code to work in the new gulp-webapp setup.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,12 @@
 /* jshint devel:true */
-console.log('\'Allo \'Allo!');
+"use strict";
+var productApp = angular.module('productApp', []);
+		
+productApp.controller('ProductsController', function ($scope) {
+	$scope.products = $DATA || [];
+	
+	$scope.orderProduct = function(product) {
+		var val = parseFloat(product.specialPrice != null ? product.specialPrice.substring(1, product.specialPrice.length) : product.regularPrice.substring(1, product.regularPrice.length));
+		return val;
+	};
+});
