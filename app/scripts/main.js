2637c492be0bbd1d68c7fb1ccb68cfe4f27d7853
mgardner <mgardner@sumtotalsystems.com>
2015-01-07 15:38:06 -0500

    change the ul to table, change the fav icon

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,11 @@
-/* jshint devel:true */
-"use strict";
-var productApp = angular.module('productApp', []);
+/* jshint strict:true, devel:true  */
+var productApp = angular.module('productApp', []); // jshint ignore:line
 		
 productApp.controller('ProductsController', function ($scope) {
-	$scope.products = $DATA || [];
+	$scope.products = $DATA || []; // jshint ignore:line
 	
 	$scope.orderProduct = function(product) {
-		var val = parseFloat(product.specialPrice != null ? product.specialPrice.substring(1, product.specialPrice.length) : product.regularPrice.substring(1, product.regularPrice.length));
+		var val = parseFloat(product.specialPrice !== null ? product.specialPrice.substring(1, product.specialPrice.length) : product.regularPrice.substring(1, product.regularPrice.length));
 		return val;
 	};
 });
